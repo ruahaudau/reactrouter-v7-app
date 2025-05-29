750e678d1e75e20d488733052734cdd5e35676f0
ruahaudau <tranhungvt@gmail.com>
2025-05-29 21:09:09 +0700

    m

--- app/components/icons/icon.component.tsx
+++ app/components/icons/icon.component.tsx
@@ -80,7 +80,7 @@ export function SettingsIcon() {
   );
 }
 
-export function SearchIcon() {
+export function SearchIcon1() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -272,3 +272,21 @@ export function CheckCircleIcon({ large }: { large?: boolean }) {
 
 
 
+export function SearchIcon() {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      fill="none" viewBox="0 0 24 24" 
+      stroke-width="1.5" 
+      stroke="currentColor" 
+      className="size-6"
+    >
+      <path 
+        stroke-linecap="round" 
+        stroke-linejoin="round" 
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" 
+      />
+    </svg>
+
+  )
+}
--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,13 +1,15 @@
 import { type PantryShelf } from "@prisma/client";
-import {  useLoaderData } from "react-router";
+import { useLoaderData } from "react-router";
 import { getAllShelves } from "../../models/pantryshelf.server";
-import classNames from 'classnames'
+import classNames from 'classnames';
+import { SearchIcon1 } from "../../components/icons/icon.component";
+import { type LoaderArgs } from "@react-router/node";
 
 type LoaderData = {
   shelves: PantryShelf[];
 };
 
-export async function loader() {
+export async function loader({request}: LoaderArgs) {
   const shelves = await getAllShelves();
   return {shelves} as LoaderData;
 }
@@ -17,7 +19,21 @@ export default function Pantry() {
   const data = useLoaderData() as LoaderData;
   return (
     <div>
-      <ul className={classNames("flex gap-8 overflow-x-auto", 'snap-x snap-mandatory md:snap-none', 'px-4')}>
+      <form 
+        className={classNames(
+          "flex border-2 border-gray-300 rounded-md", 
+          "focus-within:border-primary md:w-80 mb-3"
+        )}
+      >
+        <button className="px-2 mr-1"><SearchIcon1 /></button>
+        <input type='text' 
+          name='q'
+          autoComplete="off"
+          placeholder="Search Shelf..."
+          className="w-full py-3 px-2 outline-none" 
+        />
+      </form>
+      <ul className={classNames("flex gap-4 overflow-x-auto", 'snap-x snap-mandatory md:snap-none', 'px-4')}>
         {data.shelves.map((shelf) => (
           <li key={shelf.id} className={classNames('border-2 border-primary rounded-md p-4 h-fit', 'w-full flex-none snap-center', 'md:w-96')}>
              <h1 className='text-2xl font-semibold mb-2'>
